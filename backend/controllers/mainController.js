575306cfddec2b3b40670748dd33de5e7fb959fc
EliottMaillard25 <eliottmaillard2509@gmail.com>
2025-05-12 10:48:39 +0200

    update

--- backend/controllers/mainController.js
+++ backend/controllers/mainController.js
@@ -1,3 +1,32 @@
 exports.sayHello = (req, res) => {
     res.send('Bonjour depuis le controller !');
 };
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("loginForm");
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const username = document.getElementById("inputUser1").value;
+        const password = document.getElementById("inputPassword1").value;
+
+        fetch("/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, password }),
+        })
+        .then(response => {
+            if (response.ok) {
+                window.location.href = "/index.html";
+            } else {
+                alert("Login échoué");
+            }
+        })
+        .catch(error => {
+            console.error("Erreur lors de l'envoi :", error);
+        });
+    });
+});
