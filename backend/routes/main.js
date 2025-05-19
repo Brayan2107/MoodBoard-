a1a01607952a9e06bba2f4953a3411ae7891835e
Guzmanfjd <juandavid.guzmanfigueroa@rpn.ch>
2025-05-19 10:58:46 +0200

    Model and Post "Humeur"

--- backend/routes/main.js
+++ backend/routes/main.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const mainController = require('../controllers/mainController');
 const mailController = require('../controllers/mailController');
+const humeurController = require('../controllers/humeurController');
 
 router.get('/hello', mainController.sayHello);
+router.post('/humeur', humeurController.postHumeur);
 
 module.exports = router;
